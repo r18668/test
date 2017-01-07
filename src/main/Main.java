176e4f9775a42a323b75efd90709d67af449db78
r18668 <r18668@yandex.ru>
2017-01-07 23:13:52 +0500

    speed = 100

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,6 +3,9 @@ package main;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello " + args[0]);
+        System.out.format("Hello %s ! \n", args[0]);
+        Car car = new CarImpl();
+        car.setSpeed(200);
+        car.go();
     }
 }
