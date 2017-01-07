a82e1446e977f0408190f16cf1d2c3f3ada16501
r18668 <r18668@yandex.ru>
2017-01-07 23:14:12 +0500

    speed = 100

--- src/ru/javawebinar/webapp/model/Resume.java
+++ src/ru/javawebinar/webapp/model/Resume.java
@@ -1,9 +1,12 @@
 package ru.javawebinar.webapp.model;
 
+import java.util.Collection;
+
 /**
  * Created by 1 on 07.01.2017.
  */
 public class Resume {
     public String firstName;
-    public String lastName;
+    public String[] some;
+    Collection<String> collection;
 }
