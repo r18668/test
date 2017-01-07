92a94ff9908050eed8990aa895a89ebdfb64bde2
r18668 <r18668@yandex.ru>
2017-01-07 15:53:54 +0500

    rrrrrr

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! fhgmjh,mjh,,");
+        System.out.println("Hello World! fhgmjh,mjh,,cbnf nh h");
     }
 }
