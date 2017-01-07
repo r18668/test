0415634d473984500af246fdcaa89b816689d50b
r18668 <r18668@yandex.ru>
2017-01-07 23:13:16 +0500

    speed = 100

--- src/main/Car.java
+++ src/main/Car.java
@@ -3,10 +3,9 @@ package main;
 /**
  * Created by 1 on 07.01.2017.
  */
-public class Car {
-    int speed = 100;
+public interface Car {
+    int getSpeed();
+    void setSpeed(int speed);
 
-    void go(){
-        System.out.println("Go at speed" + speed);
-    }
+    void go();
 }
